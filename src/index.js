4a0c0b912c444d2f47c2337f65cda1681c04846a
Whispers12 <frolovandrey752@gmail.com>
2018-05-10 22:42:01 +0300

    add ws

--- src/index.js
+++ src/index.js
@@ -9,7 +9,19 @@ import { HttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { BrowserRouter } from "react-router-dom";
 import { AUTH_TOKEN } from "./constants";
-import { ApolloLink } from "apollo-client-preset";
+import { ApolloLink, split } from "apollo-client-preset";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+});
 
 const httpLink = new HttpLink({ uri: "http://localhost:4000" });
 
@@ -25,6 +37,15 @@ const middlewareAuthLink = new ApolloLink((operation, forward) => {
 });
 
 const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink);
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  httpLinkWithAuthToken
+);
 
 const client = new ApolloClient({
   link: httpLinkWithAuthToken,
